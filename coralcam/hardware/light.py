8edba461668a15b6897474e5577bca1db6176df9
Oscar Branson <oscarbranson@gmail.com>
2025-09-25 19:35:13 +0100

    fix pwm chip

--- coralcam/hardware/light.py
+++ coralcam/hardware/light.py
@@ -2,7 +2,7 @@ from rpi_hardware_pwm import HardwarePWM
 
 class Light:
     def __init__(self):
-        self.led = HardwarePWM(pwm_channel=2, hz=333, chip=2)
+        self.led = HardwarePWM(pwm_channel=2, hz=333, chip=0)
         self.brightness = 100
         
     def set_brightness(self, value=50):
